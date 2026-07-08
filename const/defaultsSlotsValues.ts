22318d09a66ac063b3291043773e9a2eec81954b
IsmVirusXD <igordesouzamendes@outlook.com>
2026-07-08 16:17:22 -0300

    feat(const): adição dos valores defaults para os Slotes

---
+++ const/defaultsSlotsValues.ts
@@ -0,0 +1,14 @@
+export const defaultSlotsByType: SlotsByType = {
+  " - ": 0,
+  "Game Finished": 1,
+  "100%": 2,
+  "All Achievement": 3,
+  "Game Completed": 4,
+};
+
+export const defaultSlotsByTime: SlotsByTime = {
+  50: 1,
+  100: 2,
+  150: 3,
+  200: 4,
+};
