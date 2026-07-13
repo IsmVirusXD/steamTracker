a9436db3d83f34a8f63a95b62fdff9331a25fa2d
IsmVirusXD <igordesouzamendes@outlook.com>
2026-07-13 11:04:06 -0300

    chore(frontend): ajustando os valores da configuraçã apos a modificação

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,5 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-import type { Metadata } from "next";
+import { Inter } from "next/font/google"; // Garanta que o import do Inter está aqui
+import { Metadata } from "next"; // Garanta que o import do Metadata está aqui
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -8,3 +7,15 @@ export const metadata: Metadata = {
   title: "Steam Tracker",
   description: "Description",
 };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-br" className={inter.variable}>
+      <body>{children}</body>
+    </html>
+  );
+}
